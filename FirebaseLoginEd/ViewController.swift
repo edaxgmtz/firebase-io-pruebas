3b45a3a01f557f4aa560c8fe1818082a5fb9ea4b
Eduardo Gonzalez <edaxgmtzbk@gmail.com>
2020-10-08 13:13:17 -0500

    Initial Commit

---
+++ FirebaseLoginEd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseLoginEd
+//
+//  Created by Miguel Ledezma on 08/10/20.
+//  Copyright © 2020 Miguel Ledezma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
